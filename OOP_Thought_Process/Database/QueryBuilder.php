b1a3fa627c0a5c76d58cb50f7f7f96197e8fd0ba
sergeysmolkinhtml <37virs85@gmail.com>
2023-07-04 10:55:16 +0200

    wip

--- OOP_Thought_Process/Database/QueryBuilder.php
+++ OOP_Thought_Process/Database/QueryBuilder.php
@@ -9,20 +9,11 @@ class QueryBuilder
         $this->pdo = $pdo;
     }
 
-    public function getAll($tableName) : bool | array
+    public function getAll(String $tableName) : Bool | Array
     {
         $statement = $this->pdo->prepare("SELECT * FROM BlogDB.{$tableName}");
         $statement->execute();
         return $statement->fetchAll(PDO::FETCH_ASSOC);
-    }
-
-    public function create(string $tableName, array $data) : void
-    {
-        $keys = implode(',', array_keys($data));
-        $tags = ":" . implode(',', array_keys($data));
-        $query = "INSERT INTO BlogDB.{$tableName} ({$keys}) VALUES ({$tags})";
-        $statement = $this->pdo->prepare($query);
-        $statement->execute($data);
     }
 
     public function getOne(String $tableName, Int $postId)
@@ -31,4 +22,41 @@ class QueryBuilder
         $statement->execute(['id' => $postId]);
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
+
+    public function create(String $tableName, Array $data) : void
+    {
+        $keys = implode(',', array_keys($data));
+        $tags = ":" . implode(',', array_keys($data));
+        $query = "INSERT INTO BlogDB.{$tableName} ({$keys}) VALUES ({$tags})";
+        $statement = $this->pdo->prepare($query);
+        $statement->execute($data);
+    }
+
+    public function update(String $tableName, Array $data, Int $postId) : void
+    {
+        #TODO bymyself
+        $keys = array_keys($data);
+        $query = '';
+
+        foreach ($keys as $key) {
+            $query .= $key . '=:' . $key . ',';
+        }
+
+        $keys = rtrim($query,',');
+        $data['id'] = $postId;
+
+        $statement = $this->pdo->prepare(
+            "UPDATE BlogDB.{$tableName} SET {$keys} WHERE BlogDB.posts.id = :id"
+        );
+
+        $statement->execute($data);
+
+    }
+
+    public function delete(String $tableName, Int $postId) : void
+    {
+        $statement = $this->pdo->prepare("DELETE FROM BlogDB.{$tableName} WHERE BlogDB.posts.id = :id");
+        $statement->execute(['id' => $postId]);
+    }
+
 }
