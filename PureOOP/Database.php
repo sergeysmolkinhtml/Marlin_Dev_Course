05fe89edcacc813e5a48f7889541364c32f23d80
sergeysmolkinhtml <37virs85@gmail.com>
2023-07-14 16:54:05 +0200

    database entity+ filter

--- PureOOP/Database.php
+++ PureOOP/Database.php
@@ -8,9 +8,11 @@ use PDOException;
 class Database
 {
     private static ?Database $instance = null;
+    private $results, $count, $query;
     private PDO $pdo;
+    private Bool $error = false;
 
-   private function __construct() {
+    private function __construct() {
        try {
            $this->pdo = new PDO('mysql:host=marl;dbname=users', 'root', '');
        } catch (PDOException $exception){
@@ -19,9 +21,50 @@ class Database
    }
    public static function getInstance() : ?Database
    {
-       if(self::$instance === null){
+       if(self::$instance === null) {
            self::$instance = new Database();
        }
        return self::$instance;
    }
+
+    public function query(String $sql, Array $params = []) : Database
+    {
+        $this->error = false;
+        $this->query = $this->pdo->prepare($sql);
+
+        if(count($params)){
+            $i = 1;
+            foreach ($params as $param) {
+                $this->query->bindValue($i, $param);
+                $i++;
+            }
+        }
+
+        if(! $this->query->execute()){
+            $this->error = true;
+        } else {
+            $this->results = $this->query->fetchAll(PDO::FETCH_OBJ);
+            $this->count = $this->query->rowCount();
+        }
+
+        return $this;
+    }
+
+    public function getErrors() : bool
+    {
+        return $this->error;
+    }
+
+    public function getResults()
+    {
+        return $this->results;
+    }
+
+    public function getCount()
+    {
+        return $this->count;
+    }
+
+   
+   
 }
--- PureOOP/index.php
+++ PureOOP/index.php
@@ -4,5 +4,15 @@ use App\Database;
 
 require_once 'Database.php';
 
-Database::getInstance();
-Database::getInstance();
+$users = Database::getInstance()->query("SELECT * FROM pureoop.users WHERE username IN (?, ?)" , ['username 1','username2']);
+/*$user1 = Database::getInstance()->get('users',['username','=','username 1']);
+$user1 = Database::getInstance()->delete('users',['username','=','username 1']);*/
+
+if($users->getErrors()){
+    echo 'We have an error';
+} else{
+    foreach ($users->getResults() as $user){
+        echo $user->username . "<br>";
+    }
+}
+
